281486c9ebdf9783b1c5e5f8b94120c40e26a217
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-09-08 09:40:19 +0200

    feat: updated

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// import Head from 'next/head'
+// import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className="bg-green-500">
+      <h2 className="text-blue-500">Hello world</h2>
+    </section>
+  )
+}
